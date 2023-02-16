d3aa763289d42b56f0e74d3ad44eb72a9a485b1e
hyeonws <hyeonws@uci.edu>
2023-02-16 13:19:31 -0800

    Completed the About Me portion; logging in working, 'Load info about me' button working properly as intended - populates the relevant fields

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-about',
@@ -11,11 +12,21 @@ export class AboutComponent implements OnInit {
   profile_link:string = null;
 
   //TODO: inject the Spotify service
-  constructor() { }
+  constructor(private service: SpotifyService) { }
 
   ngOnInit() {
+
   }
 
+  loadAbout() {
+    this.service.aboutMe().then((data) => {
+      this.name = data.name;
+      this.profile_pic = data.imageURL;
+      this.profile_link = data.spotifyProfile;
+    }, (err) => {
+      console.log("An error occured in obtaining the profile data.");
+    });
+  }
   /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
   In that function, update the name, profile_pic, and profile_link fields */
 
