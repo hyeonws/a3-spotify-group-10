b9262684e32f9809667f29cc6b8ba467cc766d5a
hyeonws <hyeonws@uci.edu>
2023-02-16 14:42:15 -0800

    Implemented the binding for search (including dropdown menu); still have to apply the actual functionality for everything

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -18,6 +18,8 @@ export class AboutComponent implements OnInit {
 
   }
 
+  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
+  In that function, update the name, profile_pic, and profile_link fields */
   loadAbout() {
     this.service.aboutMe().then((data) => {
       this.name = data.name;
@@ -27,7 +29,5 @@ export class AboutComponent implements OnInit {
       console.log("An error occured in obtaining the profile data.");
     });
   }
-  /*TODO: create a function which gets the "about me" information from Spotify when the button in the view is clicked.
-  In that function, update the name, profile_pic, and profile_link fields */
 
 }
